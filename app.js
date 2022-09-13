78a313b762f2fcf6dba8987dab0028ffefe06e0b
captainco <nguyenquocco@gmail.com>
2022-09-13 23:16:28 +0700

    update port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const indexRouter                    = require('./index');
 const express                        = require("express");
 const http                           = require("http");
 const app                            = express();
-const port                           = process.env.port || 8080;
+const port                           = process.env.PORT || 8080;
 app.use('/', indexRouter);
 app.set('port', port);
 const server                         = http.createServer(app);

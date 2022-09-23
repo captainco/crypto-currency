ac0e840d0fcbe6c2e9bef21db7d84c2f48c8b7f3
captainco <nguyenquocco@gmail.com>
2022-09-23 18:31:16 +0700

    fixed number

--- binance/binanceTest.js
+++ binance/binanceTest.js
@@ -22,8 +22,8 @@ async function Main() {
                 const price = await binance.SpotPositionRisk();
                 const Ps = (await binance.FuturesPositionRisk(symbol))[0];
                 const priceSpot = price.BTCUSDT;
-                const spFt = priceSpot - Ps.markPrice;
-                await telegram.logAlert(symbol, rsi, iconLongShort, common.ConvertToPositiveNumber(totalValue), priceSpot, Ps.markPrice, spFt);
+                const spFt = (priceSpot - Ps.markPrice).toFixed(2);
+                await telegram.logAlert(symbol, rsi, iconLongShort, common.ConvertToPositiveNumber(totalValue), (priceSpot).toFixed(2), (Ps.markPrice).toFixed(2), spFt);
             }
         } catch (e) {
             await telegram.log(`⚠ ${e}`);
@@ -39,8 +39,8 @@ async function Main() {
                     const price = await binance.SpotPositionRisk();
                     const Ps = (await binance.FuturesPositionRisk('BTCUSDT'))[0];
                     const priceSpot = price.BTCUSDT;
-                    const spFt = priceSpot - Ps.markPrice;
-                    await telegram.logAlert('BTCUSDT', rsi, '⚪', 'Chưa xác định', priceSpot, Ps.markPrice, spFt);
+                    const spFt = (priceSpot - Ps.markPrice).toFixed(2);
+                    await telegram.logAlert('BTCUSDT', rsi, '⚪', 'Chưa xác định', (priceSpot).toFixed(2), (Ps.markPrice).toFixed(2), spFt);
                 }
             }
         } catch (e) {

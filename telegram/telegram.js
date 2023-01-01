502f6fb83d32347ea17b3da3aa3ade5e52d51dce
captainco <nguyenquocco@gmail.com>
2023-01-01 15:00:21 +0700

    update wh

--- telegram/telegram.js
+++ telegram/telegram.js
@@ -111,7 +111,7 @@ bot.command('d', async (ctx) => {
 
 bot.command('wh', async (ctx) => {
     if (!IsMyTelegramAccount(ctx)) return;
-    ctx.reply(`🤖 Webhook hiện tại: ${process.env.Webhookud_}`);
+    ctx.reply(`🤖 Webhook hiện tại: ${process.env.Webhookud}`);
 });
 
 bot.on('sticker', (ctx) => {
